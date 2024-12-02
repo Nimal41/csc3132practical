e445bd40f9a2d1c6d84f53771b7f2b35408efa7c
Nimal41 <Nimal2000thas17@gmail.com>
2024-12-02 15:20:10 +0000

    getable file created

---
+++ practicals/22_11practical/dbconf.php
@@ -0,0 +1,22 @@
+<?php
+define('SERVERNAME', '127.0.0.1');
+define('USERNAME', 'root');
+define('PASSWORD', 'mariadb');
+define('DBNAME', 'students');
+try {
+	//connect with database
+	$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+	if (!$connect) {
+		die("connection failed".mysqli_connect_error());
+	} 
+	else {
+		echo "Connected successfully<br>";
+	}
+} 
+catch (Exception $e) {
+	die($e->getMessage());
+}
+
+
+
+?>
